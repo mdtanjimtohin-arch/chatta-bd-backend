8f09b29b27ea7026297cf885605dc3e1a4f37e8f
mdtanjimtohin-arch <mdtanjimtohin@gmail.com>
2026-09-12 08:59:14 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+const express = require("express");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Easy Builder BD Server is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
